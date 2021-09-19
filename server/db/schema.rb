55b3044a544acfc550295f612a8bbe1ed8ef83e4
atsuya02 <atsuya0930@yahoo.co.jp>
2021-09-20 00:10:46 +0900

    fix: アーティストモデルのnameカラムのnullをfalseに修正

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_09_19_144105) do
 
   create_table "artists", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
