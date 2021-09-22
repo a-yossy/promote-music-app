641d0dcf03affc015c028f014eacbd338b420ce6
atsuya02 <atsuya0930@yahoo.co.jp>
2021-09-22 16:03:35 +0900

    feat: ログイン時と非ログイン時のヘッダーを変えるため、Header.tsxを修正

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,18 +1,37 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { User } from 'lib/user';
+import { useLocation } from 'react-router';
 
-const Header: FC = () => (
-  <header>
-    <h1>
-      <Link to="/">Promote Music App</Link>
-    </h1>
-    <div>
-      <Link to="signup">ユーザー作成</Link>
-    </div>
-    <div>
-      <Link to="login">ログイン</Link>
-    </div>
-  </header>
-);
+const Header: FC = () => {
+  const [user, setUser] = useState<User | undefined>();
+  const location = useLocation();
+
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem('loginUser') || '[]'));
+  }, [location]);
+
+  return (
+    <header>
+      <h1>
+        <Link to="/">Promote Music App</Link>
+      </h1>
+      {user?.name ? (
+        <div>
+          <Link to="/">ログアウト</Link>
+        </div>
+      ) : (
+        <>
+          <div>
+            <Link to="signup">ユーザー作成</Link>
+          </div>
+          <div>
+            <Link to="login">ログイン</Link>
+          </div>
+        </>
+      )}
+    </header>
+  );
+};
 
 export default Header;
