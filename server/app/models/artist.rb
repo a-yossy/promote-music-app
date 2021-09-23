27937119169f8cbd0728bd3b66a3fb5cbf2a93fc
atsuya02 <atsuya0930@yahoo.co.jp>
2021-09-24 01:03:41 +0900

    feat: アーティストモデルのnameを一意にするため、artist.rbを修正

--- server/app/models/artist.rb
+++ server/app/models/artist.rb
@@ -1,5 +1,5 @@
 class Artist < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   has_many :user_artists
   has_many :users, through: :user_artists
 end
