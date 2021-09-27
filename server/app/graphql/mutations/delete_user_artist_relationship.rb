e83de3bcf0c0fbe5f2872134bed6458c7f4ce0d0
atsuya02 <atsuya0930@yahoo.co.jp>
2021-09-27 23:09:21 +0900

    refactor: フォロー解除のif文をdestroyで分岐できるよう修正

--- server/app/graphql/mutations/delete_user_artist_relationship.rb
+++ server/app/graphql/mutations/delete_user_artist_relationship.rb
@@ -18,9 +18,8 @@ module Mutations
     def resolve(user_name:, artist_name:)
       user = User.find_by(name: user_name)
       artist = Artist.find_by(name: artist_name)
-      if unfollow = UserArtist.find_by(user: user, artist: artist)
-        unfollow.destroy
-      else
+      user_artist_relationship = UserArtist.find_by(user: user, artist: artist)
+      if !user_artist_relationship.destroy
         raise GraphQL::ExecutionError, "Not following"
       end
     end
