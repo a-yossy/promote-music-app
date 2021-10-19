9e2a8192ea6a3b249f330380957a70dad5131fad
atsuya02 <atsuya0930@yahoo.co.jp>
2021-10-20 03:05:08 +0900

    refactor: artistPageコンポーネントから使用していないReactのimportを削除

--- client/src/pages/artistPage.tsx
+++ client/src/pages/artistPage.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState, useRef } from 'react';
+import { FC, useState, useRef } from 'react';
 import { useQuery, useMutation, ApolloError } from '@apollo/client';
 import ArtistsCard from 'components/ArtistsCard';
 import {
