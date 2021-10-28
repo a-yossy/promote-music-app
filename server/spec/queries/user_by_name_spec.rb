de2328401f09c2c2b31498ea1f831b2366842115
atsuya02 <atsuya0930@yahoo.co.jp>
2021-10-28 23:39:53 +0900

    test: user_by_nameクエリのテスト作成

---
+++ server/spec/queries/user_by_name_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :request do
+  describe "user_by_name_query" do
+    query_string = <<-GRAPHQL
+      query($name: String!) {
+        userByName(name: $name) {
+          id
+          name
+        }
+      }
+    GRAPHQL
+
+    let(:user) { create(:user) }
+
+    it do
+      result = ServerSchema.execute(query_string, variables: { name: user.name })
+      user_result = result['data']['userByName']
+      expect(user_result['name']).to eq user.name
+    end
+  end
+end
