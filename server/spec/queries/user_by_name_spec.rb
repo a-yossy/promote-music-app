a906f88dcc089dad915cd939fc74717f7af6c94a
atsuya02 <atsuya0930@yahoo.co.jp>
2021-10-30 00:02:06 +0900

    test: Rspecでの代入の記述をletを使った記述に修正

--- server/spec/queries/user_by_name_spec.rb
+++ server/spec/queries/user_by_name_spec.rb
@@ -2,20 +2,21 @@ require 'rails_helper'
 
 RSpec.describe User, type: :request do
   describe "user_by_name_query" do
-    query_string = <<-GRAPHQL
-      query($name: String!) {
-        userByName(name: $name) {
-          id
-          name
+    let(:query_string) {
+      <<-GRAPHQL
+        query($name: String!) {
+          userByName(name: $name) {
+            id
+            name
+          }
         }
-      }
-    GRAPHQL
-
+      GRAPHQL
+    }
     let(:user) { create(:user) }
+    let(:result) { ServerSchema.execute(query_string, variables: { name: user.name }) }
+    let(:user_result) { result['data']['userByName'] }
 
     it 'should return right user' do
-      result = ServerSchema.execute(query_string, variables: { name: user.name })
-      user_result = result['data']['userByName']
       expect(user_result['name']).to eq user.name
     end
   end
