803169aafd5b537298768e9c11588a4a8ff9ea5f
atsuya02 <atsuya0930@yahoo.co.jp>
2021-10-30 00:53:39 +0900

    test: user_by_nameテストのlet文を修正

--- server/spec/queries/user_by_name_spec.rb
+++ server/spec/queries/user_by_name_spec.rb
@@ -12,23 +12,23 @@ RSpec.describe User, type: :request do
         }
       GRAPHQL
     }
-    let(:user) { create(:user) }
+    let!(:user) { create(:user, name: 'user1') }
+    let(:result) { ServerSchema.execute(query_string, variables: { name: name }) }
+    let(:result_data) { result['data'] }
 
     context 'when user exists' do
-      let(:result) { ServerSchema.execute(query_string, variables: { name: user.name }) }
-      let(:user_result) { result['data']['userByName'] }
+      let(:name) { 'user1' }
 
       it 'should return right user' do
-        expect(user_result['name']).to eq user.name
+        expect(result_data['userByName']['name']).to eq 'user1'
       end
     end
 
     context 'when user does not exist' do
-      let(:result) { ServerSchema.execute(query_string, variables: { name: 'sarueru' }) }
-      let(:user_result) { result['data'] }
+      let(:name) { 'sarueru' }
 
       it 'should return nil' do
-        expect(user_result).to eq nil
+        expect(result_data).to eq nil
       end
     end
   end
