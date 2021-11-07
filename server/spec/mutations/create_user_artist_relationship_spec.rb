cc9272eeb3b127f6fdc6d9d76d3e3ef18c33e5be
atsuya02 <atsuya0930@yahoo.co.jp>
2021-11-08 02:41:26 +0900

    test: create_user_artist_relationshipミューテーションのテスト作成

---
+++ server/spec/mutations/create_user_artist_relationship_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe UserArtist, type: :request do
+  describe 'create_user_artist_relationship_mutation' do
+    let(:query_string) {
+      <<-GraphQl
+        mutation ($userName: String!, $artistName: String!) {
+          createUserArtistRelationship(input: { userName: $userName, artistName: $artistName }) {
+            user {
+              id
+              name
+            }
+          }
+        }
+      GraphQl
+    }
+    let!(:user) { create(:user, name: 'user') }
+    let!(:artist) { create(:artist, name: 'artist') }
+    subject { ServerSchema.execute(query_string, variables: { userName: 'user', artistName: 'artist' }) }
+
+    context 'when user does not follow artist' do
+      it 'should count of UserArtist increases 1' do
+        expect { subject }.to change{ UserArtist.count }.by(1)
+      end
+    end
+
+    context 'when user already follows artist' do
+      let!(:user_artist) { create(:user_artist, user: user, artist: artist) }
+
+      it 'should count of UserArtist does not change' do
+        expect { subject }.to change{ UserArtist.count }.by(0)
+      end
+    end
+  end
+end
