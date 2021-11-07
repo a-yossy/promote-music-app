e5c8c011d5fac6a3a20e1888cd183a4f0694db91
atsuya02 <atsuya0930@yahoo.co.jp>
2021-11-08 02:15:00 +0900

    test: create_userミューテーションのテスト作成

---
+++ server/spec/mutations/create_user_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :request do
+  describe 'create_user_mutation' do
+    let(:query_string) {
+      <<-GraphQL
+        mutation ($name: String!) {
+          createUser(input: { name: $name }) {
+            user {
+              id
+              name
+            }
+          }
+        }
+      GraphQL
+    }
+    let(:result) { ServerSchema.execute(query_string, variables: { name: name }) }
+    let(:result_data) { result['data']['createUser'] }
+
+    context 'when user name is valid' do
+      let(:name) { 'user' }
+
+      it 'should return right user' do
+        expect(result_data['user']['name']).to eq 'user' and change{ User.count }.by(1)
+      end
+    end
+
+    context 'when same user name already exists' do
+      let!(:other_user) { create(:user, name: 'user') }
+      let(:name) { 'user' }
+
+      it 'should return nil' do
+        expect(result_data).to eq nil and change{ User.count }.by(0)
+      end
+    end
+
+    context 'when user name is nil' do
+      let(:name) { '' }
+
+      it 'should return nil' do
+        expect(result_data).to eq nil and change{ User.count }.by(0)
+      end
+    end
+  end
+end
