43f0a83b0a3cb453d4b7dc2ab094a1fc3b42117e
atsuya02 <atsuya0930@yahoo.co.jp>
2021-11-09 17:37:24 +0900

    test: テスト名修正

--- client/src/__tests__/ArtistListElement.test.tsx
+++ client/src/__tests__/ArtistListElement.test.tsx
@@ -1,7 +1,7 @@
 import rerender from 'react-test-renderer';
 import ArtistListElement from 'components/ArtistListElement';
 
-it('render correcyly', () => {
+it('<ArtistListElement artist={{ id: 1, name: "artist" }} /> should render artist', () => {
   const tree = rerender
     .create(<ArtistListElement artist={{ id: 1, name: 'artist' }} />)
     .toJSON();
