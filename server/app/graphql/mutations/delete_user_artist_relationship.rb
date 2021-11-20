afe0d56f38be62e51f8646f8bd8f7d9f180acf84
atsuya02 <atsuya0930@yahoo.co.jp>
2021-11-21 00:49:02 +0900

    refactor: フォロー解除のミューテーションの条件分岐を修正

--- server/app/graphql/mutations/delete_user_artist_relationship.rb
+++ server/app/graphql/mutations/delete_user_artist_relationship.rb
@@ -19,8 +19,10 @@ module Mutations
       user = User.find_by(name: user_name)
       artist = Artist.find_by(name: artist_name)
       user_artist_relationship = UserArtist.find_by(user: user, artist: artist)
-      if !user_artist_relationship || !user_artist_relationship.destroy
+      if !user_artist_relationship
         raise GraphQL::ExecutionError, "Not following"
+      else
+        user_artist_relationship.destroy!
       end
     end
   end
